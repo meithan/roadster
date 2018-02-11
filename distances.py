037bae31bee785431422b303e28451a3d18a7e07
Meithan West <meithan@gmail.com>
2018-02-10 22:38:38 -0600

    Add code to plot Roadster distances

--- distances.py
+++ distances.py
@@ -1,3 +1,4 @@
+# Plots the Roadster's distances to the Sun, the Earth and Mars vs time
 import locale
 from math import sqrt
 from datetime import datetime, timedelta
@@ -30,18 +31,24 @@ _, Earth_pos,_ = read_JPL_data("horizons_Earth.txt")
 _, Mars_pos,_ = read_JPL_data("horizons_Mars.txt")
 Mars_dist = []
 Earth_dist = []
+Mars_xy = []
+Mars_z = []
 for i in range(len(dates)):
   x,y,z = Roadster_pos[i]
   Ex,Ey,Ez = Earth_pos[i]
   Mx,My,Mz = Mars_pos[i]
   Mars_dist.append(sqrt((x-Mx)**2+(y-My)**2+(z-Mz)**2))
+  Mars_z.append(abs(z-Mz))
+  Mars_xy.append(sqrt((x-Mx)**2+(y-My)**2))
   Earth_dist.append(sqrt((x-Ex)**2+(y-Ey)**2+(z-Ez)**2))
 
 plt.figure(figsize=(12,4))
 plt.title("Roadster's distance to ...")
 plt.plot(dates, Roadster_helio, "-", color="k", label="Sun")
-plt.plot(dates, Mars_dist, "-", color="r", label="Mars")
 plt.plot(dates, Earth_dist, "-", color="b", label="Earth")
+plt.plot(dates, Mars_dist, "-", color="r", label="Mars")
+plt.plot(dates, Mars_xy, "--", color="r", label="Mars xy")
+plt.plot(dates, Mars_z, ":", color="r", label="Mars z")
 
 plt.axhline(0,ls=":",color="k")
 plt.legend(loc="upper left")
